707d7ff0be25d986c1a9d603edaebb2179056146
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-14 04:35:25 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,4 @@
 def greeting(name)
-  name = name.strip #repeat yourself to pass the test
+  name = name.strip #repeating myself to pass the test?
   puts "Hello #{name}. It's nice to meet you."
 end
